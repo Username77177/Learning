8b72f601cbd853b6e1904d3460649d99457e909e
Username77177 <Username77177@gmail.com>
2020-03-05 23:49:07 +0300

    05/03/2020 23:48:57 | C++ Learning Start

---
+++ cpp/1. Старт.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello, World" << endl; // Вывод сообщения
+    // Endl - перенос следующей строки (если она есть)
+
+    int main_num; // Объявление переменной
+    // int - целочисленная перменная
+
+    cin >> main_num; // Ввод данных в переменную
+
+    cout << main_num << "\n"; // Вывод переменной, а также ещё один способ закончить строку
+    return 0;
+}
